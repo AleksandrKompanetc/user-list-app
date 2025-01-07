75146f1cbb489024e2521c31933afe27ffc39036
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-07 23:26:10 +0200

    added input and button for adding new users

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
   return (
     <div className="App">
       <div>
-        
+        <input 
+          type="text"
+          placeholder='Enter new user name...'
+          value={newUser}
+          onChange={(e) => setNewUser(e.target.value)} 
+        />
+        <button>Add User</button>
       </div>
     </div>
   );

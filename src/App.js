e35629a7caa04e9d68c0d17e9684e7cacf3a06ac
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-07 23:33:02 +0200

    adding fetch request for getting users

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 function App() {
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState('');
-  
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+      setUsers(response.data)
+    })
+    .catch((error) => console.error('Error fetching users:', error));
+  }, []);
+
   return (
     <div className="App">
       <h1>User List</h1>

4f111a28a1860bc109579f650d0fb8f441103ebe
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-07 23:23:08 +0200

    adding axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        
+      </div>
     </div>
   );
 }
